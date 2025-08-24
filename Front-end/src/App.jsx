6ad5ec79dbf747cf1ef0fa7b337f7b80123de301
wcoder547 <mw5667155@gmail.com>
2025-08-24 20:00:28 +0500

    Implement routing and lazy loading for Home, Login, Chat, and Group pages

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {lazy} from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+const Home = lazy(() => import('./pages/Home'))
+const Login = lazy(() => import('./pages/Login'))
+const Chat = lazy(() => import('./pages/Chat'))
+const Group = lazy(() => import('./pages/Group'))
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat/:chatid" element={<Chat />} />
+        <Route path="/group" element={<Group />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {CssBaseline} from "@mui/material"
+import {HelmetProvider} from "react-helmet-async"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CssBaseline />
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
